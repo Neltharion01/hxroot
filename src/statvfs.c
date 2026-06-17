0496aec90d2b81f3dd55e232dc0815c099b8922b
Neltharion <NtNeltharion@gmail.com>
2026-06-18 01:51:40 +0300

    Added statfs stub

--- src/statvfs.c
+++ src/statvfs.c
@@ -1,7 +1,13 @@
+#include <sys/vfs.h>
 #include <sys/statvfs.h>
 #include <dlfcn.h>
+#include <stdlib.h>
 
 #include "hxroot.h"
+
+int statfs(const char *path, struct statfs *buf) {
+    eprintf("UNIMPLEMENTED SHIT! statfs\n"); abort();
+}
 
 int (*statvfs_real)(const char *restrict path, struct statvfs *restrict buf);
 int statvfs(const char *restrict path, struct statvfs *restrict buf) {
