ef4d9903c0adb30a24792b760a16032b418d25b2
Neltharion <NtNeltharion@gmail.com>
2026-06-25 15:00:28 +0300

    Implemented renameat2

--- src/rename.c
+++ src/rename.c
@@ -1,8 +1,10 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 #include "hxroot.h"
 
@@ -64,6 +66,31 @@ int renameat(int oldfd, const char *old, int newfd, const char *new) {
     return renameat_real(oldfd, new_old, newfd, new_new);
 }
 
+int (*renameat2_real)(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, unsigned int flags);
 int renameat2(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, unsigned int flags) {
-    eprintf("UNIMPLEMENTED SHIT! renameat2\n"); abort();
+    if(!renameat2_real) renameat2_real = dlsym(RTLD_NEXT, "renameat2");
+    HxInit();
+
+    if(HxL2s && !(flags & RENAME_NOREPLACE)) {
+        struct stat st = {0};
+        int ret = stat(newpath, &st);
+        if(ret == -1 && errno != ENOENT) return -1;
+        if(ret == 0 && st.st_mode & S_IFLNK) {
+            // Unlink destination because it could be a "hardlink"
+            // Regular symlink would have been overwritten anyway
+            ret = unlink(newpath);
+            if(ret == -1 && errno != ENOENT) return -1;
+        }
+    }
+
+    int oldlen = HxL(oldpath);
+    char oldpathbuf[oldlen];
+    const char *new_oldpath = HxExpandPath(oldpathbuf, oldpath);
+
+    int newlen = HxL(newpath);
+    char newpathbuf[newlen];
+    const char *new_newpath = HxExpandPath(newpathbuf, newpath);
+
+    if(HxDebug) eprintf("renameat2(%d, \"%s\" -> \"%s\", %d, \"%s\" -> \"%s\", %d)\n", olddirfd, oldpath, new_oldpath, newdirfd, newpath, new_newpath, flags);
+    return renameat2_real(olddirfd, new_oldpath, newdirfd, new_newpath, flags);
 }
