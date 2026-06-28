7f7fc443de2507f04fdbf4076a6d03945aa181ff
Neltharion <NtNeltharion@gmail.com>
2026-06-28 19:12:25 +0300

    Implemented scandir

---
+++ src/scandir.c
@@ -0,0 +1,28 @@
+#include <dirent.h>
+#include <dlfcn.h>
+
+#include "hxroot.h"
+
+int (*scandir_real)(const char *restrict dirp, struct dirent ***restrict namelist, typeof(int (const struct dirent *)) *filter, typeof(int (const struct dirent **, const struct dirent **)) *compar);
+int scandir(const char *restrict dirp, struct dirent ***restrict namelist, typeof(int (const struct dirent *)) *filter, typeof(int (const struct dirent **, const struct dirent **)) *compar) {
+    if(!scandir_real) scandir_real = dlsym(RTLD_NEXT, "scandir");
+    HxInit();
+
+    int len = HxL(dirp);
+    char pathbuf[len];
+    const char *new_dirp = HxExpandPath(pathbuf, dirp);
+
+    return scandir_real(new_dirp, namelist, filter, compar);
+}
+
+int (*scandirat_real)(int dirfd, const char *restrict dirp, struct dirent ***restrict namelist, typeof(int (const struct dirent *)) *filter, typeof(int (const struct dirent **, const struct dirent **)) *compar);
+int scandirat(int dirfd, const char *restrict dirp, struct dirent ***restrict namelist, typeof(int (const struct dirent *)) *filter, typeof(int (const struct dirent **, const struct dirent **)) *compar) {
+    if(!scandirat_real) scandirat_real = dlsym(RTLD_NEXT, "scandirat");
+    HxInit();
+
+    int len = HxL(dirp);
+    char pathbuf[len];
+    const char *new_dirp = HxExpandPath(pathbuf, dirp);
+
+    return scandirat_real(dirfd, new_dirp, namelist, filter, compar);
+}
