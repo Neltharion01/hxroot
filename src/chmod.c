8870cfd24c3ac25e11b36fe6b6e6cbdd54214850
Neltharion <NtNeltharion@gmail.com>
2026-07-23 10:14:47 +0300

    Implemented lchmod

--- src/chmod.c
+++ src/chmod.c
@@ -17,6 +17,19 @@ int chmod(const char *path, mode_t mode) {
     return chmod_real(new_path, mode);
 }
 
+static int (*lchmod_real)(const char *path, mode_t mode);
+int lchmod(const char *path, mode_t mode) {
+    if(!lchmod_real) lchmod_real = dlsym(RTLD_NEXT, "lchmod");
+    HxInit();
+
+    int len = HxL(path);
+    char pathbuf[len];
+    const char *new_path = HxExpandPath(pathbuf, path);
+
+    if(HxDebug) eprintf("lchmod(\"%s\" -> \"%s\", %o)\n", path, new_path, mode);
+    return lchmod_real(new_path, mode);
+}
+
 static int (*fchmodat_real)(int fd, const char *path, mode_t mode, int flag);
 int fchmodat(int fd, const char *path, mode_t mode, int flag) {
     if(!fchmodat_real) fchmodat_real = dlsym(RTLD_NEXT, "fchmodat");
